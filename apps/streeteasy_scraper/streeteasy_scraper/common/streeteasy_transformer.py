3c81cbc020e032e613ac0c50174c7180cf2a05c2
nedhmn <hermannned@gmail.com>
2025-05-02 19:19:30 -0400

    chore: streeteasy-transformer init

---
+++ apps/streeteasy_scraper/streeteasy_scraper/common/streeteasy_transformer.py
@@ -0,0 +1,18 @@
+from db.models import Address
+
+
+class StreetEasyTransformer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_address_data() -> Address:
+        pass
+
+    @staticmethod
+    def _has_active_listing() -> bool:
+        pass
+
+    @staticmethod
+    def _is_address_match() -> bool:
+        pass
