f315b16573c0368b19ed88361e9cb871657b702a
nedhmn <hermannned@gmail.com>
2025-05-02 07:45:23 -0400

    feat: webhook post route

--- apps/webhook/webhook/core/models.py
+++ apps/webhook/webhook/core/models.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 
 class HealthCheckResponse(BaseModel):
     status: str
+
+
+class WebhookPayload(BaseModel):
+    status: int
+    response_id: str
+    request_url: str
