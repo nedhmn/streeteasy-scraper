ea339a10efb39b749f9b836500b1fe79f504b88c
nedhmn <hermannned@gmail.com>
2025-05-02 07:54:15 -0400

    refactor: rename repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# StreetEasy Scraper
+# StreetEasy Scraper Monorepo
 
 <div align="center" style="margin-bottom: 20px">
     <img src="./docs/src/assets/scraper-hero.png" alt="Scraper mascot hero section" height="350px">
--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nedhmn.github.io",
-  base: "/streeteasy-scraper/",
+  base: "/streeteasy-scraper-monorepo/",
   integrations: [
     starlight({
       title: "My Docs",
