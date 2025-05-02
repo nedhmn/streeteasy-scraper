d769fe8e4802fcb7f9ff71aa57f4566a021db7ea
nedhmn <hermannned@gmail.com>
2025-05-02 03:42:00 -0400

    lint: fix output type

--- packages/db/db/init_db.py
+++ packages/db/db/init_db.py
@@ -10,7 +10,7 @@ setup_logger()
 logger = logging.getLogger("db.main")
 
 
-async def init_db():
+async def init_db() -> None:
     async with async_engine.begin() as conn:
         logger.info("Attempting to create all database tables...")
         await conn.run_sync(Base.metadata.create_all)
