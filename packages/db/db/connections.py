2e1ee6b2290cfee3d6d96f3796d2af365038e602
nedhmn <hermannned@gmail.com>
2025-05-04 01:08:12 -0400

    feat: config engine

--- packages/db/db/connections.py
+++ packages/db/db/connections.py
@@ -8,7 +8,9 @@ from sqlalchemy.orm import Session, sessionmaker
 from db.config import settings
 
 # Sync engine
-sync_engine = create_engine(settings.SYNC_DATABASE_URL, pool_pre_ping=True)
+sync_engine = create_engine(
+    settings.SYNC_DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20
+)
 sync_db_session = sessionmaker(bind=sync_engine, autocommit=False, autoflush=False)
 
 
@@ -26,7 +28,9 @@ def get_db_session() -> Generator[Session, None, None]:
 
 
 # Async engine
-async_engine = create_async_engine(settings.ASYNC_DATABASE_URL)
+async_engine = create_async_engine(
+    settings.ASYNC_DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20
+)
 async_db_session = async_sessionmaker(
     bind=async_engine, expire_on_commit=False, class_=AsyncSession
 )
