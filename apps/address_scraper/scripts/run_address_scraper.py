3cbc1f2633a66502902dc7b88a6125fb8225bde4
nedhmn <hermannned@gmail.com>
2025-05-04 01:08:59 -0400

    feat: is_db_seeded func to avoid duplicating db on prestart script

--- apps/address_scraper/scripts/run_address_scraper.py
+++ apps/address_scraper/scripts/run_address_scraper.py
@@ -2,13 +2,13 @@ import asyncio
 import logging
 
 import httpx
-from db.connections import get_async_db_session
-from db.models import Address
-from sqlalchemy.ext.asyncio import AsyncSession
-from utils.logging import setup_logger
-
 from address_scraper.address_extractor import AddressExtractor
 from address_scraper.config import settings
+from db.connections import get_async_db_session
+from db.models import Address
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from utils.logging import setup_logger
 
 setup_logger("address_scraper.log")
 logger = logging.getLogger("scripts.run_address_scraper")
@@ -17,6 +17,11 @@ logger = logging.getLogger("scripts.run_address_scraper")
 async def main() -> None:
     logger.info("Starting address scraping process.")
     async with httpx.AsyncClient() as http_client, get_async_db_session() as db_session:
+        # Check if database already has addresses (strong signal script ran already)
+        if await is_db_seeded(db_session):
+            logger.info("Database already has addresses! Exiting...")
+            return
+
         # Extract addresses
         logger.info("Extracting addresses from source.")
         extractor = AddressExtractor(settings, http_client, db_session)
@@ -27,6 +32,11 @@ async def main() -> None:
         logger.info("Loading addresses to database.")
         await load_addresses_to_db(input_addresses, db_session)
         logger.info("Addresses loaded to database successfully.")
+
+
+async def is_db_seeded(db_session: AsyncSession) -> bool:
+    result = await db_session.execute(select(Address.id).limit(1))
+    return result.first() is not None
 
 
 async def load_addresses_to_db(
